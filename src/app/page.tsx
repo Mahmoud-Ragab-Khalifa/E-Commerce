6a64f69dde4fda1be2afd42250ab019711825407
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-01-21 06:33:13 +0200

    handle project files structure

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <h1 className="text-9xl">Hello</h1>
-      <h1 className="text-9xl">hello</h1>
-      <h1 className="text-9xl">محمود</h1>
-    </>
-  );
-}
