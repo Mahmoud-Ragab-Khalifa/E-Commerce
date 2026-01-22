182f669dc326abbba2e4dedd2066ce6ac0147e96
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-01-23 01:36:02 +0200

    add the main color to be global in tailwind theme

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,7 +13,7 @@ const Header = () => {
     <div className="bg-[#1F2937] p-2.5">
       <div className="container mx-auto flex justify-between items-center gap-4">
         <div className="text-[13px] text-white flex items-center gap-2 overflow-hidden">
-          <span className="bg-[#111827] py-0.5 px-3 rounded-3xl overflow-hidden whitespace-nowrap text-ellipsis">
+          <span className="bg-main-color py-0.5 px-3 rounded-3xl overflow-hidden whitespace-nowrap text-ellipsis">
             {t("hot")}
           </span>
           <p className="overflow-hidden whitespace-nowrap text-ellipsis">
