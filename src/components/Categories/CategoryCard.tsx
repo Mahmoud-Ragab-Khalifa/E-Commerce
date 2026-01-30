f4a552711bbad3c66d51ec1c8b08a9eb388c9e5b
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-01-30 02:56:07 +0200

    handle responsive design by reflect the hover effect in desktop to be active touch on mobile

--- src/components/Categories/CategoryCard.tsx
+++ src/components/Categories/CategoryCard.tsx
@@ -10,7 +10,7 @@ const CategoryCard = ({ category, image }: CategoryCardProbs) => {
   return (
     <div className="group bg-card dark:bg-card rounded-md text-center p-4 max-h-62.5">
       <Image
-        className="mx-auto transition-transform duration-300 group-hover:scale-[1.1] group-hover:rotate-[-5deg]"
+        className="mx-auto transition-transform duration-300 group-hover:scale-[1.1] group-hover:rotate-[-5deg] group-active:scale-[1.1] group-active:rotate-[-5deg]"
         alt={"gaming"}
         src={image}
         width={150}
@@ -19,8 +19,11 @@ const CategoryCard = ({ category, image }: CategoryCardProbs) => {
       <Button
         variant={"default"}
         size={"lg"}
-        className="w-full font-bold! bg-neutral-200 text-light group-hover:bg-light dark:text-dark
-          transition-colors duration-500 group-hover:text-neutral-100 group-hover:dark:bg-[#253853] dark:bg-accent hover:bg-light/95"
+        className="w-full font-bold transition-colors duration-500
+           text-light dark:text-dark
+           group-hover:text-neutral-100 group-active:text-neutral-100 group-hover:dark:bg-[#253853]
+           bg-neutral-200 dark:bg-accent group-hover:bg-light group-active:bg-light 
+           hover:bg-light/95 group-active:dark:bg-[#253853]"
       >
         {category}
       </Button>
