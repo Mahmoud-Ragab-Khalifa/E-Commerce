f2f9d73b93aa257877e54c334ac85d07aba7673d
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-02-06 19:38:19 +0200

    Activate add to favourites feature

--- src/components/ProductsList/ProductCardActions.tsx
+++ src/components/ProductsList/ProductCardActions.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { useState } from "react";
 import { Button } from "../ui/button";
-import { Eye, Heart } from "lucide-react";
+import { Eye } from "lucide-react";
+
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 const ProductCardActions = () => {
+  const [active, setactive] = useState(false);
+
   return (
     <>
       <Button
@@ -11,12 +19,18 @@ const ProductCardActions = () => {
       >
         <Eye />
       </Button>
+
       <Button
         className="cursor-pointer absolute z-50 right-1 md:-right-full md:group-hover:right-1 transition-all duration-500 top-8 hover:bg-neutral-200 delay-200"
         variant={"ghost"}
         size={"icon-sm"}
+        onClick={() => setactive(!active)}
       >
-        <Heart />
+        {active ? (
+          <FavoriteIcon sx={{ fontSize: 17, color: "#ff6d75" }} />
+        ) : (
+          <FavoriteBorderIcon sx={{ fontSize: 17 }} />
+        )}
       </Button>
     </>
   );
