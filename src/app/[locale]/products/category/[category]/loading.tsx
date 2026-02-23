ec2d19f4a8559e1269576561975ca0a0de7f1e1b
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-02-23 15:09:41 +0200

    Add simple loading spinner to category and checkout pages

---
+++ src/app/[locale]/products/category/[category]/loading.tsx
@@ -0,0 +1,9 @@
+const CategoryLoading = () => {
+  return (
+    <div className="flex items-center justify-center h-[calc(100vh-200px)]">
+      <div className="w-8 h-8 border-4 border-muted border-t-primary rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default CategoryLoading;
