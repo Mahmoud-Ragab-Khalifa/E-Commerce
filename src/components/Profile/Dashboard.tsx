1240995fb7504ba2a452c5ebd7cfd016590968a2
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-02-28 19:06:14 +0200

    fix a11y on profile dashboard tabs and handle logout action

--- src/components/Profile/Dashboard.tsx
+++ src/components/Profile/Dashboard.tsx
@@ -1,19 +1,27 @@
+"use client";
+
 import { useTranslations } from "next-intl";
 import DashboardList from "./DashboardList";
+import { useAuthStore } from "@/store/authStore";
 
 const Dashboard = () => {
   const t = useTranslations("profile");
+
+  const logout = useAuthStore((state) => state.logout);
 
   return (
     <div className="flex flex-col gap-6">
       <DashboardList title="dashboard" />
       <DashboardList title="settings" />
 
-      <div className="mx-5 relative py-2 px-4 rounded-md ring-1 ring-destructive transition-all duration-300 hover:ring-2 focus:ring-2 group text-center overflow-hidden z-10 cursor-pointer hover:text-white">
+      <button
+        className="mx-5 relative py-2 px-4 rounded-md ring-1 ring-destructive transition-all duration-300 hover:ring-2 focus:ring-2 focus:outline-none group text-center overflow-hidden z-10 cursor-pointer hover:text-white"
+        onClick={logout}
+      >
         <span>{t("logout")}</span>
 
-        <span className="absolute start-0 top-0 h-full w-0 group-hover:w-full bg-destructive transition-all duration-300 -z-10" />
-      </div>
+        <span className="absolute start-0 top-0 h-full w-0 group-hover:w-full bg-destructive transition-all duration-300 -z-10 group-focus:w-full" />
+      </button>
     </div>
   );
 };
--- src/components/Profile/DashboardList.tsx
+++ src/components/Profile/DashboardList.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { useState } from "react";
-
 import { dataList } from "@/lib/dashboard";
 import { useTranslations } from "next-intl";
 import { Geist } from "next/font/google";
@@ -29,20 +27,21 @@ const DashboardList = ({ title }: DashboardListProbs) => {
       </h1>
       <div className="flex flex-col">
         {targetList.map(({ icon: Icon, title, number }) => (
-          <div
-            className={`flex items-center gap-1.5 focus:ring-2 transition-all duration-300 focus:ring-neutral-300 cursor-pointer py-4 px-5 relative group hover:text-light dark:hover:text-dark ${active === title ? "text-light dark:text-dark" : "text-light/70 dark:text-dark/60"}`}
+          <button
+            className={`focus:outline-none focus-visible:rounded-md focus-visible:mx-2 flex items-center gap-1.5 focus-visible:ring-2 transition-all duration-300 focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-700 cursor-pointer py-4 px-5 relative group hover:text-light dark:hover:text-dark focus-visible:text-light dark:focus-visible:text-dark 
+            ${active === title ? "text-light dark:text-dark" : "text-light/70 dark:text-dark/60"}`}
             key={title}
             onClick={() => setActive(title)}
           >
             <span
-              className={`absolute w-1 start-0 bottom-0 bg-main-color dark:bg-[#253853] group-hover:h-full transition-all duration-300 ${active === title ? "h-full" : "h-0"}`}
+              className={`absolute w-1 start-0 bottom-0 bg-main-color dark:bg-[#253853] group-hover:h-full transition-all duration-300 ${active === title ? "h-full" : "h-0"} group-focus-visible:h-0`}
             />
             <Icon size={18} />
             <div className="data flex flex-1 justify-between items-center">
               <span className="text-sm">{t(title)}</span>
               <span className={`${geist.className} text-sm`}>{number}</span>
             </div>
-          </div>
+          </button>
         ))}
       </div>
     </div>
